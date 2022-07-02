dc4d3b703da8b96a84c9a91f598c50a9e19e151f
FranciscoRodriguez44 <frankrodrigu6@gmail.com>
2022-07-01 20:54:22 -0600

    se agrega validacionlog y compraetc

--- components/view/LoginView.js
+++ components/view/LoginView.js
@@ -1,36 +1,72 @@
-import { StyleSheet, Text, View, Button,Image,TextInput } from "react-native";
-import Flatbutton5 from "../../button/buttonlogin";
-const LoginView = (props) => {
+
+import React from 'react';
+
+import { StyleSheet, Text, View, ActivityIndicator, TextInput, ScrollView, Button,Image } from "react-native";
+import Flatbutton5, {} from "../../button/buttonlogin";
+
+const DatosLogin ={usuario: 'GabrielGay', password:'selacome'}
+
+class LoginView extends React.Component{
+constructor(props){
+	super(props);
+	this.state={
+		usuario :'',//aqui declaro lo estados
+		password:'',
+	}
+}
+render(){
 	return (
-		<View style={styles.container}>
-			<Image
-			style={{width:480,height:700,position:"absolute"}} 
-			source={require('../../components/img/libreriaMA.jpg')}
-			
-			
-			/>
-				
-				<View styles ={styles.OptionContainer}>
-				<Text style={styles.Title}>LIBRERIA MARIA AUXILIADORA</Text>
+			<View style={styles.container}>
+				<Image
+				style={{width:480,height:700,position:"absolute"}} 
+				source={require('../../components/img/libreriaMA.jpg')}
+				/>
+					<View styles ={styles.OptionContainer}>
+						<Text style={styles.Title}>LIBRERIA MARIA AUXILIADORA</Text>
+		
+				<TextInput style = {styles.input} placeholder = "USUARIO" 
+				//se le pasa el estado a input para que reconozca el estado(state),
+						onChangeText={(usuario) => this.setState({usuario})}
+					value={this.state.usuario}/>
+		               <TextInput style = {styles.input} placeholder = "PASSWORD"
+					   secureTextEntry
+					   onChangeText={(password) => this.setState({password})}
+					   value={this.state.password} />
+	
+		
+		               <Flatbutton5 text="LOGIN IN"
+					   onPress={this._login}
+					   // onPress={()=>{  this.props.navigation.navigate('Home')}}
+		               />
+					
+						
+						</View>
+					
+					
+	 		</View>
+			);
+	 };
 
-				<TextInput style = {styles.input} placeholder = "USER" />
-                <TextInput style = {styles.input} placeholder = "PASSWORD" />
+	 //se hace una funcion 
+	 _login=async()=>{
+		 //verificamos que si nuestro datologin de  usuario y contraseña  coincide con el estado 
+		 if(DatosLogin.usuario === this.state.usuario&& DatosLogin.password === this.state.password)
+		 //si coinciden usuari y contra
+		 {
+			this.props.navigation.navigate('Home')
+			// alert('Logged in');
+		 }else{
+			 //si no coinciden los datos 
+			 alert('usuario o password incorrectos');
+		 }
+             //return;
+	 }
+	 
+		
+}
 
 
-               <Flatbutton5 text="LOGIN IN" onPress={()=>{
-                    props.navigation.navigate('Home')
-               }}
-               />
-            
-				
-				</View>
-			
-            
-		</View>
-	);
-};
-
-export { LoginView };
+export {LoginView}
 
 const styles = StyleSheet.create({
 	container: {
